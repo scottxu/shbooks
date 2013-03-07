f7317fe8b575be1f5d298e0486b5ad84583e296c
scottxu <xubingchao@gmail.com>
2013-03-07 23:04:58 +0800

    shbooks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shbooks::Application.config.secret_token = '50706bc9276a4b74e6411b2fc67bcc2dbb4f4882eadc2bef34ac430fe36b458d62752d71006af706e46694569c574c29d54d2107f63d9b7beb2c0ed3d3449d4e'
